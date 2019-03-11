890a112befe65f295196e050e77c836fe73cde4b
Joel DeSante <yooogle@Joels-iMac.fios-router.home>
2019-03-10 22:54:30 -0400

    Fixed Major Bug (Blocks spawning above ground) and also implemented degrdation paths

--- src/main/java/com/yooogle/ruggedtrail/listener/BlockStepListener.java
+++ src/main/java/com/yooogle/ruggedtrail/listener/BlockStepListener.java
@@ -1,17 +1,18 @@
 package com.yooogle.ruggedtrail.listener;
 
+import java.util.List;
 import java.util.Random;
 
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.data.type.Farmland;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
 
 import com.yooogle.ruggedtrail.Main;
 
@@ -56,26 +57,16 @@ public class BlockStepListener implements Listener {
 		}
 		
 		// Gets the block directly under the player
-		Location below = new Location(event.getPlayer().getWorld(), event.getTo().getX(), Math.ceil(event.getTo().getY() - 1), event.getTo().getZ());
+		Location below = player.getLocation().add(new Vector(0,-1,0));
 		
-		// Checks to see if the below block is air, if so then see if the next block is not
-		// (Makes it better for sprint jumping players)
+		// Return if below block is air
 		if (below.getBlock().getType() == Material.AIR) {
-			below = new Location(event.getPlayer().getWorld(), event.getTo().getX(), Math.ceil(event.getTo().getY() - 3), event.getTo().getZ());
-			
-			//Check if the block is under another block
-			Material above = below.getWorld().getBlockAt(new Location(below.getWorld(), below.getX(), below.getY() + 1, below.getZ())).getType();
-			if (above != Material.AIR && above != Material.WATER && above != Material.LAVA) {
-				// Something important is above this
-				return;
-			}
-			
+			return;	
 		}
-		
-		//System.out.print(Arrays.toString(default_path.toArray()));
 		
 		Block block = below.getBlock();
 		Material type = block.getType();
+		plugin.getLogger().info(type.toString());
 		
 		// Finds the distance that the player has traveled
 		double distance = Math.sqrt(
@@ -85,115 +76,20 @@ public class BlockStepListener implements Listener {
 				);
 		
 		
-		Material[] changable = {
-				Material.STONE,
-				Material.COBBLESTONE,
-				Material.GRAVEL,
-				Material.GRASS_BLOCK,
-				Material.ANDESITE,
-				Material.DIORITE,
-				Material.GRANITE,
-				Material.COARSE_DIRT,
-				Material.DIRT,
-				Material.GRASS_PATH
-		};
-		
 		// Checks for 5/1000 chances
 		float random = new Random().nextFloat();
 		
 		float probability = config_prob;
-		
-		
-		
+				
 		if (random <= probability && distance > 0) {
 			
 			if (canChange("default_path", type)) {
-				
-				/*switch(type) {
-					
-					// Set to Cobblestone 
-					case STONE:
-						block.setType(Material.COBBLESTONE);
-						break;
-						
-					// This section all turns to coarse dirt
-					case COBBLESTONE:
-						block.setType(Material.COARSE_DIRT);
-						break;
-						
-					case GRAVEL:
-						block.setType(Material.COARSE_DIRT);
-						break;
-						
-					case GRASS_BLOCK:
-						block.setType(Material.COARSE_DIRT);
-						break;
-						
-					case ANDESITE:
-						block.setType(Material.COARSE_DIRT);
-						break;
-						
-					case DIORITE:
-						block.setType(Material.COARSE_DIRT);
-						break;
-						
-					case GRANITE:
-						block.setType(Material.COARSE_DIRT);
-						break;
-						
-					// Turn to Dirt
-					case COARSE_DIRT:
-						block.setType(Material.DIRT);
-						break;
-						
-					case DIRT:
-						if (plugin.getConfig().getBoolean("enable_grass_paths") == true) {
-							// Turn to path
-							block.setType(Material.GRASS_PATH);
-						}
-						break;
-						
-					// Padded Path
-					case GRASS_PATH:
-						if (plugin.getConfig().getBoolean("enable_mudding") == true) {
-							// Allow paths to turn to mud
-							if (event.getPlayer().getWorld().hasStorm() == true) {
-								
-								block.setType(Material.FARMLAND);
-								
-								// Set up the data values
-								Farmland m_data = (Farmland) block.getState().getBlockData();
-								m_data.setMoisture(7);
-								
-								// Commit data values
-								block.setBlockData(m_data);
-								
-							}
-						}
-						break;
-					
-					// Do Nothing
-					default:
-						break;
-				}*/
-				switchType("default_path", type);
+				block.setType(switchType("default_path", type));
 			}
 		}
 	}
 	
 	/* Utils */
-	
-	@Deprecated
-	// Checks if an array has the material we are looking for
-	private Boolean hasMaterial(Material[] arr, Material type) {
-		for (int i = 0; i < arr.length; i++) {
-			if (type == arr[i]) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
 	
 	private boolean canChange(String path, Material mat) {
 		ConfigurationSection sec = plugin.getConfig().getConfigurationSection(path);
@@ -213,13 +109,12 @@ public class BlockStepListener implements Listener {
 		for (String key : sec.getKeys(false)) {
 			if (original.toString().equalsIgnoreCase(key)) {
 				
-				ConfigurationSection deep_sec = plugin.getConfig().getConfigurationSection(path + "." + key);
-				
+				List<String> deep_sec = sec.getStringList(key);
 				Random r = new Random();
-				int deep_sec_len = deep_sec.getKeys(false).toArray().length - 1;
+				int deep_sec_len = deep_sec.toArray().length;
 				int rand_val = (int)(r.nextFloat() * deep_sec_len);
 				
-				return Material.getMaterial(deep_sec.getKeys(false).toArray()[rand_val].toString());
+				return Material.getMaterial(deep_sec.toArray()[rand_val].toString());
 				
 			}
 		}
